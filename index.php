876755930da43e742d75d01a38855bee81279b14
Yonhanafi14 <yonhanafi@gmail.com>
2022-10-29 11:15:51 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,14 @@
 </head>
 <body>
     <h1> hello world</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem animi ipsa non quasi consequatur quos. Totam nostrum illum officia corrupti odit eum, velit, pariatur eveniet incidunt odio quisquam quam perspiciatis.
+    </p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+
+    </ul>
 </body>
 </html>
